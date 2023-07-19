b14121e7ac9e02706b7760033f757545d7bcfcf0
Shubhra <ishubhra@outlook.com>
2023-07-19 00:30:14 -0700

    DeepAR Init

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import * as deepar from "deepar";
+import Carousel from "./carousel.js";
+
+// Log the version. Just in case.
+console.log("Deepar version: " + deepar.version);
+
+// Top-level await is not supported.
+// So we wrap the whole code in an async function that is called immediatly.
+(async function () {
+  // Resize the canvas according to screen size and pixel ratio.
+  const canvas = document.getElementById("deepar-canvas");
+  const dpr = window.devicePixelRatio || 1;
+  canvas.width = Math.floor(window.innerWidth * dpr);
+  canvas.height = Math.floor(window.innerHeight * dpr);
+
+  // trigger loading progress bar animation
+  const loadingProgressBar = document.getElementById("loading-progress-bar");
+  loadingProgressBar.style.width = "100%";
+
+  // All the effects are in the public/effects folder.
+  // Here we define the order of effect files.
+  const effectList = [
+    "effects/ray-ban-wayfarer.deepar",
+    "effects/sunglass_filter.deepar",
+    "effects/shoe_tryon.deepar"
+  ];
+
+  let deepAR = null;
+
+  // Initialize DeepAR with an effect file.
+  try {
+    deepAR = await deepar.initialize({
+      licenseKey: "d636f5f81777539ab7c65b5a1df6a918ef1e7fc4e3dc0fd21a6d44cb02b19eb66b2133b7b1b0cf42",
+      canvas,
+      effect: effectList[0],
+      // Removing the rootPath option will make DeepAR load the resources from the JSdelivr CDN,
+      // which is fine for development but is not recommended for production since it's not optimized for performance and can be unstable.
+      // More info here: https://docs.deepar.ai/deepar-sdk/deep-ar-sdk-for-web/download-optimizations#custom-deployment-of-deepar-web-resources
+      rootPath: "./deepar-resources",
+      additionalOptions:{
+        cameraConfig:{
+            facingMode:"environment",
+        },
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    document.getElementById("loading-screen").style.display = "none";
+    document.getElementById("permission-denied-screen").style.display = "block";
+    return;
+  }
+
+  var resizeCanvas = function () {
+    const canvas = document.getElementById("deepar-canvas");
+    const dpr = window.devicePixelRatio || 1;
+    canvas.width = Math.floor(window.innerWidth * dpr);
+    canvas.height = Math.floor(window.innerHeight * dpr);
+  };
+
+  window.addEventListener("resize", resizeCanvas);
+
+  // Hide the loading screen.
+  document.getElementById("loading-screen").style.display = "none";
+  document.getElementById("ar-screen").style.display = "block";
+
+  window.effect = effectList[0];
+
+  const glassesCarousel = new Carousel("carousel");
+  glassesCarousel.onChange = async (value) => {
+    const loadingSpinner = document.getElementById("loading-spinner");
+
+    if (window.effect !== effectList[value]) {
+      loadingSpinner.style.display = "block";
+      await deepAR.switchEffect(effectList[value]);
+      window.effect = effectList[value];
+    }
+    loadingSpinner.style.display = "none";
+  };
+})();
